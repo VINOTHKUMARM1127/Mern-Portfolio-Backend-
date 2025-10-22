f308c315bcb88c2313ea353eec6170fb1d6ca0fa
VINOTHKUMARM1127 <vinothmaddy061011@gmail.com>
2025-10-22 17:44:49 +0530

    Adding Minor Changes

--- Models/Skils.js
+++ Models/Skils.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
 const SkillsSchema = new mongoose.Schema({
-    Skill : {type:String , required:true},
-    Icon : {type:String , required:true},
-    Category : {type:String, required:true}
+    Skill : {type: String , required:true},
+    Icon : {type: String , required:true},
+    Category : {type: String, required:true},
+    Order : {type: Number, required:true},
 });
 
 const Skills = new mongoose.model("SkillsSchema",SkillsSchema);
--- Routes/SkillsRoute.js
+++ Routes/SkillsRoute.js
@@ -2,50 +2,53 @@ const express = require("express");
 const router = express.Router();
 const Skills = require("../Models/Skils.js");
 
-router.get(("/get-skills"), async (req,res)=>{
-    try{
-         const SkillsList = await Skills.find();
-        res.status(200).json(SkillsList)
-    }catch(err){
-        res.status(400).json(err)
-    }
+router.get("/get-skills", async (req, res) => {
+  try {
+    const SkillsList = await Skills.find();
+    res.status(200).json(SkillsList);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.post(("/add-skills"), async (req,res)=>{
-    try {
-        const {Skill,Icon} = req.body;
-        const newSkills = new Skills({
-            Skill,Icon,Category
-        })
-        await newSkills.save();
-        res.status(200).json("Skill Added")
-        
-    } catch (err) {
-
-        res.status(400).json(err)
-        
-    }
+router.post("/add-skills", async (req, res) => {
+  try {
+    const { Skill, Icon, Category, Order } = req.body;
+    const newSkills = new Skills({
+      Skill,
+      Icon,
+      Category,
+      Order,
+    });
+    await newSkills.save();
+    res.status(200).json("Skill Added");
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put(("/update-skills/:id"), async (req,res)=>{
-    try {
-        const {Skill,Icon,Category} = req.body;
-        await Skills.findByIdAndUpdate(req.params.id, {
-            Skill,Icon,Category
-        })
-        res.status(200).json("Skill Updated")
-    } catch (err) {
-        res.status(400).json(err)
-    }
+router.put("/update-skills/:id", async (req, res) => {
+  try {
+    const { Skill, Icon, Category, Order } = req.body;
+    await Skills.findByIdAndUpdate(req.params.id, {
+      Skill,
+      Icon,
+      Category,
+      Order,
+    });
+    res.status(200).json("Skill Updated");
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.delete(("/delete-skills/:id"), async (req,res)=>{
-    try {
-        await Skills.findByIdAndDelete(req.params.id);
-        res.status(200).json("Skill Deleted")
-    } catch (err) {
-        res.status(400).json(err)
-    }
+router.delete("/delete-skills/:id", async (req, res) => {
+  try {
+    await Skills.findByIdAndDelete(req.params.id);
+    res.status(200).json("Skill Deleted");
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-module.exports = router
+module.exports = router;
